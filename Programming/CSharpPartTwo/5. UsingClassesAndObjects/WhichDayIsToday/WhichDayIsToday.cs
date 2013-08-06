e75d63caca5d8bf4a4a6bcc3e60181588d7bb4bd
Martin Anastasov <m4rt00@gmail.com>
2013-08-06 09:47:34 +0300

    update

---
+++ Programming/CSharpPartTwo/5. UsingClassesAndObjects/WhichDayIsToday/WhichDayIsToday.cs
@@ -0,0 +1,23 @@
+﻿// 3. Write a program that prints to the console which day of the week is today. Use System.DateTime.
+
+using System;
+
+class WhichDayIsToday
+{
+    static void PrintDay()
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Black;
+        DayOfWeek today = DateTime.Today.DayOfWeek;
+        Console.WriteLine(" Today is {0} ", today);
+
+        //DateTime today = DateTime.Now;
+        //Console.WriteLine("Today is {0}", today.ToString("dddd"));
+        Console.ResetColor();
+    }
+
+    static void Main()
+    {
+        PrintDay();
+    }
+}

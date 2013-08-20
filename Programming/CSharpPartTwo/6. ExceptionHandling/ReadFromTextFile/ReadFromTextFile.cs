f8299094895b3fb90f0fb23157c3348709c79e62
Martin Anastasov <m4rt00@gmail.com>
2013-08-20 15:12:53 +0300

    commit exception handling homework

---
+++ Programming/CSharpPartTwo/6. ExceptionHandling/ReadFromTextFile/ReadFromTextFile.cs
@@ -0,0 +1,41 @@
+﻿/*
+3. Write a program that enters file name along with its full file path (e.g. C:\WINDOWS\win.ini), 
+reads its contents and prints it on the console. 
+Find in MSDN how to use System.IO.File.ReadAllText(…). 
+Be sure to catch all possible exceptions and print user-friendly error messages.
+*/
+
+using System;
+using System.IO;
+
+class ReadFromTextFile
+{
+    static void ReadFile(string filePath)
+    {
+        StreamReader dataFile = new StreamReader(filePath);
+
+        using (dataFile)
+        {
+            string fileContents = dataFile.ReadToEnd();
+            Console.WriteLine(fileContents);
+        }
+    }
+
+    static void Main()
+    {
+        string filePath = @"..\.\data.txt";
+        // Console.Write("Enter file path and name: ");
+        // string filePath = Console.ReadLine();
+
+        try
+        {
+            ReadFile(filePath);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Exception cought!");
+            Console.WriteLine(exception.GetType().Name);
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
---
+++ Programming/CSharpPartTwo/6. ExceptionHandling/ReadNumbers/ReadNumbers.cs
@@ -0,0 +1,41 @@
+﻿// 2. Write a method ReadNumber(int start, int end) that enters an integer number in given range [start…end]. 
+//      If an invalid number or non-number text is entered, the method should throw an exception. 
+//      Using this method write a program that enters 10 numbers:
+//      a1, a2, … a10, such that 1 < a1 < … < a10 < 100
+
+using System;
+
+class ReadNumbers
+{
+    static int ReadNumber(int start, int end)
+    {
+            int number = int.Parse(Console.ReadLine());
+            if (!(start < number && number < end))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return number;
+    }
+
+    static void Main()
+    {
+        try
+        {
+            int start = 1;
+            int end = 100;
+            for (int i = 0; i < 10; i++)
+            {
+                start = ReadNumber(start, end);
+            }
+        }
+        catch (FormatException)
+        {
+            Console.Error.WriteLine("Invalid number!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.Error.WriteLine("Invalid number!");
+        }        
+    }
+}

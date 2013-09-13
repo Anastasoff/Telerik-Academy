3e4eeda985213b55d36479bebc24d62ec77d97f7
Martin Anastasov <m4rt00@gmail.com>
2013-09-13 20:25:46 +0300

    BIG Update
    Added Team Project and Exam Preparations

---
+++ Programming/CSharpPartTwo/10.ExamPreparation/ExamPrep-4.Feb.2013/ConsoleJustification/ConsoleJustification.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ConsoleJustification
+{
+    static void Main()
+    {
+        int linesCount = int.Parse(Console.ReadLine());
+        int maxLineLength = int.Parse(Console.ReadLine());
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < linesCount; i++)
+        {
+            string readCurrentLine = Console.ReadLine();
+            builder.Append(readCurrentLine.Trim() + " ");
+        }
+
+        string[] currentLineWords = builder.ToString().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        List<string> listOfWords = new List<string>(currentLineWords);
+        do
+        {
+            builder.Clear();
+            builder.Append(listOfWords[0]);
+            int currentWord = 1;
+            while (builder.Length < maxLineLength && currentWord < listOfWords.Count)
+            {
+                builder.Append(" " + listOfWords[currentWord]);
+                currentWord++;
+            }
+
+            if (builder.Length > maxLineLength)
+            {
+                builder.Remove(builder.Length - listOfWords[currentWord - 1].Length - 1, listOfWords[currentWord - 1].Length + 1);
+                currentWord--;
+            }
+
+            if (builder.Length == maxLineLength || currentWord == 1)
+            {
+                Console.WriteLine(builder);
+                listOfWords.RemoveRange(0, currentWord);
+                continue;
+            }
+
+            int spacecount = currentWord - 1;
+            int sizeOfSpaces = (maxLineLength - builder.Length + spacecount) / spacecount;
+            int numOfLongSpaces = (maxLineLength - builder.Length + spacecount) % spacecount;
+
+            builder.Clear();
+            builder.Append(listOfWords[0]);
+            for (int i = 1; i <= spacecount; i++)
+            {
+                if (i <= numOfLongSpaces)
+                {
+                    builder.Append(" ");
+                }
+
+                builder.Append(new string(' ', sizeOfSpaces) + listOfWords[i]);
+            }
+
+            listOfWords.RemoveRange(0, currentWord);
+            Console.WriteLine(builder);
+        }
+        while (listOfWords.Count > 0);
+    }
+}
---
+++ Programming/CSharpPartTwo/10.ExamPreparation/ExamPrep-5.Feb.2013/DurankulakNumbers/DurankulakNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DurankulakNumbers
+{
+    private static int GetDurankulakNumbers(string number)
+    {
+        Dictionary<string, int> durankulakNumbers = new Dictionary<string, int>();
+
+        for (int i = 0; i < 26; i++)
+        {
+            durankulakNumbers.Add(((char)(i + 65)).ToString(), i);
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 26; j++)
+            {
+                durankulakNumbers.Add(((char)(i + 97)).ToString() + ((char)(j + 65)).ToString(), durankulakNumbers.Count);
+            }
+        }
+
+        int result = -1;
+        if (durankulakNumbers.ContainsKey(number))
+        {
+            result = durankulakNumbers[number];
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        string number = Console.ReadLine();
+        List<int> list = new List<int>();
+
+        string currentNumber = string.Empty;
+        int result = 0;
+        for (int i = 0; i < number.Length; i++)
+        {
+            currentNumber += number[i];
+
+            if (((char)currentNumber[currentNumber.Length - 1]) <= 'Z')
+            {
+                result = GetDurankulakNumbers(currentNumber);
+                list.Add(result);
+                result = 0;
+                currentNumber = string.Empty;
+            }
+        }
+
+        ulong decimalResult = 0;
+        for (int i = 0; i < list.Count; i++)
+        {
+            decimalResult += (ulong)list[i] * (ulong)Math.Pow(168, list.Count - 1 - i);
+        }
+
+        Console.WriteLine(decimalResult);
+    }
+}

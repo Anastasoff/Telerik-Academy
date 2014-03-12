456c65050f99015ae855335672951f35d30b2095
Martin Anastasov <m4rt00@gmail.com>
2014-03-12 21:18:12 +0200

    Update

---
+++ Programming/HighQualityCode/10.CodeTuningAndOptimization/PerformanceComparer/SortingAlgorithms/SortingAlgorithms.cs
@@ -0,0 +1,18 @@
+﻿// * Write a program to compare the performance of insertion sort, selection sort, quicksort for int, double and string values. 
+//  Check also the following cases: random values, sorted values, values sorted in reversed order.
+namespace SortingAlgorithms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class SortingAlgorithms
+    {
+        static void Main(string[] args)
+        {
+            // TODO: implement some classes with extended methods of each algorithm
+        }
+    }
+}

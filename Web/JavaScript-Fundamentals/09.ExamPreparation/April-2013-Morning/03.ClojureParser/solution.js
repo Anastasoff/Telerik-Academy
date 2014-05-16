80e68ae97201906b876ebe99d089b336731c8444
Martin Anastasov <m4rt00@gmail.com>
2014-05-16 21:02:38 +0300

    update

--- Web/JavaScript-Fundamentals/09.ExamPreparation/April-2013-Morning/03.ClojureParser/solution.js
+++ Web/JavaScript-Fundamentals/09.ExamPreparation/April-2013-Morning/03.ClojureParser/solution.js
@@ -1,44 +1,105 @@
 ﻿function Solve(inputs) {
-    // asociativen masiv za definiraite funcii ili masiv kato opa6ka
-    // opa6ka za rezultat
-    var funcs = {};
-    for (var i = 0; i < inputs.length; i++) {
+    function calculateExpression(expression) {
+        var result = 0;
+        var values = [];
+        var i = 2;
+        var operator = expression[i - 1];
+        for (i = 2; i < expression.length - 1; i++) {
+            if (expression[i] !== ' ' && expression[i] !== ')') {
+                var tempStr = '';
 
-        var command = inputs[i];
-        for (var j = 1; j < command.length - 1; j++) {
-            var currentDef = command.indexOf('def');
-            if (currentDef >= 0) {
-                var index = 'def'.length + 1; // !
-                var funcName = '';
-                var funcValue = 0;
-                var inWord = false;
-                while (true) {
+                while (expression[i] !== ' ' && expression[i] !== ')') {
+                    tempStr += expression[i];
+                    i++;
+                }
 
-                    if (command[index] !== ' ') {
-
-
-                        inWord = true;
+                var value = parseInt(tempStr);
+                if (isNaN(value)) {
+                    for (var f in funcs) {
+                        if (tempStr == f) {
+                            value = parseInt(funcs[f]);
+                        }
                     }
-
-                    index++;
-
-                    inWord = false;
                 }
 
+                values.push(value);
+                i--;
+            }
+        }
 
-                while (command[index] !== ' ' && command[index] !== ')' && command[index] !== '(') {
-                    funcValue += parseInt(command[index]);
-                    index++;
-                }
-
-                funcs[funcName] = funcValue;
-
-                j = index; // ? +- 1
+        result = values[0];
+        if (operator === '+') {
+            for (i = 1; i < values.length; i++) {
+                result += values[i];
+            }
+        } else if (operator === '-') {
+            for (i = 1; i < values.length; i++) {
+                result -= values[i];
+            }
+        } else if (operator === '*') {
+            for (i = 1; i < values.length; i++) {
+                result *= values[i];
+            }
+        } else if (operator === '/') {
+            for (i = 1; i < values.length; i++) {
+                result /= values[i];
             }
 
+            result = Math.floor(result);
+        }
 
+        return result;
+    }
+
+    var output,
+        funcs = {};
+    var tempOutput = 0;
+    for (var j = 0; j < inputs.length; j++) {
+        var command = inputs[j];
+        var defIndex = command.indexOf('def');
+        if (defIndex > 0) {
+            var funcName = '';
+            var funcValue = 0;
+            var index = defIndex + 3;
+
+            // get function name
+            while (true) {
+                if (command[index] !== ' ') {
+                    funcName += command[index];
+                    if (command[index + 1] === ' ') {
+                        index++;
+                        break;
+                    }
+                }
+                index++;
+            }
+
+            // get value or expression
+            var tempValue = '';
+            while (true) {
+                if (command[index] !== ' ' && command[index] !== '(') {
+                    tempValue = command.substring(index, command.length - 1).trim();
+                    funcValue = parseInt(tempValue);
+                    break;
+                }
+
+                if (command[index] === '(') {
+                    tempValue = command.substring(index, command.length - 1).trim();
+                    funcValue = calculateExpression(tempValue);
+                    break;
+                }
+
+                index++;
+            }
+
+            funcs[funcName] = funcValue;
+        } else {
+            tempOutput += calculateExpression(command);
         }
     }
 
-    return;
+    // !!!
+    output = tempOutput;
+
+    return output;
 }

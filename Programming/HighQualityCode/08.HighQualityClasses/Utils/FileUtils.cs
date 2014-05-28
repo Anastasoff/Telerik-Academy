dd1ec657d9e1defbd1d076ed78e5bc3af5b0ad89
Martin Anastasov <m4rt00@gmail.com>
2014-05-28 17:21:28 +0300

    work on high quality classes homework

---
+++ Programming/HighQualityCode/08.HighQualityClasses/Utils/FileUtils.cs
@@ -0,0 +1,67 @@
+﻿// ********************************
+// <copyright file="FileUtils.cs" company="Telerik Academy">
+// Copyright (c) 2014 Telerik Academy. All rights reserved.
+// </copyright>
+//
+// ********************************
+namespace Utils
+{
+    using System;
+
+    /// <summary>
+    /// Performs operations on <see cref="System.String"/> instances
+    /// that contain file or directory path information.
+    /// </summary>
+    public static class FileUtils
+    {
+        /// <summary>
+        /// Returns the extension of the specified path string.
+        /// </summary>
+        public static string GetFileExtension(string path)
+        {
+            CheckInputPath(path);
+
+            int lastDotIndex = path.LastIndexOf(".");
+            if (lastDotIndex == -1)
+            {
+                return string.Empty;
+            }
+
+            string extension = path.Substring(lastDotIndex + 1);
+
+            return extension;
+        }
+
+        /// <summary>
+        /// Returns the file name of the specified path string without the extension.
+        /// </summary>
+        public static string GetFileNameWithoutExtension(string path)
+        {
+            CheckInputPath(path);
+
+            int lastDotIndex = path.LastIndexOf(".");
+            if (lastDotIndex == -1)
+            {
+                return path;
+            }
+
+            string fileNameWithoutExtension = path.Substring(0, lastDotIndex);
+
+            return fileNameWithoutExtension;
+        }
+
+
+        private static void CheckInputPath(string path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException("Path is null.");
+            }
+
+            if (path.Length == 0)
+            {
+                throw new ArgumentException("Path is empty.");
+            }
+        }
+    }
+}
---
+++ Programming/HighQualityCode/08.HighQualityClasses/UtilsDemo/RectangularCuboid.cs
@@ -0,0 +1,72 @@
+﻿namespace UtilsDemo
+{
+    using System;
+
+    public class RectangularCuboid
+    {
+        private double width;
+        private double height;
+        private double depth;
+
+        public RectangularCuboid(double width, double height, double depth)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Depth = depth;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("RectangularCuboid's width cannot be less or equal to zero.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("RectangularCuboid's height cannot be less or equal to zero.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double Depth
+        {
+            get
+            {
+                return this.depth;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("RectangularCuboid's depth cannot be less or equal to zero.");
+                }
+
+                this.depth = value;
+            }
+        }
+    }
+}

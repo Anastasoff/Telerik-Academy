544a40b53093d133f7e9557e1777134cd118f492
Martin Anastasov <m4rt00@gmail.com>
2014-05-30 14:36:05 +0300

    up

---
+++ Web/JavaScript-DOM-And-UI/01.DocumentObjectModel/Source/scripts/get-color-from-input.js
@@ -0,0 +1,5 @@
+﻿function onGetColorBtnClick() {
+    var color = document.getElementById('color').value;
+
+    document.body.style.background = color;
+}
---
+++ Web/JavaScript-DOM-And-UI/01.DocumentObjectModel/Source/scripts/get-text-from-input.js
@@ -0,0 +1,9 @@
+﻿function onGetValueBtnClick() {
+    var inputStr = document.getElementById('inputText').value;
+
+    var content = document.createElement('p');
+    content.innerHTML = inputStr;
+
+    var result = document.getElementById('result');
+    result.appendChild(content);
+}

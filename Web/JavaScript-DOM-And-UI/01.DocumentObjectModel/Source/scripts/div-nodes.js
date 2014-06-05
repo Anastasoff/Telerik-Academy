ca9b7280a9cebf9a8d03f318d3724476daf6d2ad
Martin Anastasov <m4rt00@gmail.com>
2014-06-05 11:08:28 +0300

    update

---
+++ Web/JavaScript-DOM-And-UI/01.DocumentObjectModel/Source/scripts/div-nodes.js
@@ -0,0 +1,25 @@
+﻿usingQuerySelectorAll();
+console.log('\n');
+usingGetElementsByTagName();
+
+function usingQuerySelectorAll() {
+    console.log('Create a function using querySelectorAll()');
+
+    var selectedDivs = document.querySelectorAll('div > div');
+
+    for (var i = 0, length = selectedDivs.length; i < length; i++) {
+        console.log(selectedDivs[i]);
+    }
+}
+
+function usingGetElementsByTagName() {
+    console.log('Create a function using getElementsByTagName()');
+
+    var allDivs = document.getElementsByTagName('div');
+
+    for (var i = 0, length = allDivs.length; i < length; i++) {
+        if (allDivs[i].parentNode.nodeName === 'DIV') {
+            console.log(allDivs[i]);
+        }
+    }
+}

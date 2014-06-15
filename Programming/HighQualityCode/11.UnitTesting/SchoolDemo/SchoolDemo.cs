669eb46dc558c500a430c2222e2272af8dd9f2f4
Martin Anastasov <m4rt00@gmail.com>
2014-06-15 19:46:45 +0300

    update

---
+++ Programming/HighQualityCode/11.UnitTesting/SchoolDemo/SchoolDemo.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolDemo
+{
+    using School;
+    public class SchoolDemo
+    {
+        public static void Main(string[] args)
+        {
+            School school = new School();
+            Course course = new Course();
+            Student student = new Student("John", school);
+        }
+    }
+}

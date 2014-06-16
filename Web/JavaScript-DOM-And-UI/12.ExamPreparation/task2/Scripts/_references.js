c1ba1c4a469bd7bf3b73ae9e4d9338cbed58fdbd
Martin Anastasov <m4rt00@gmail.com>
2014-06-16 22:08:51 +0300

    update

---
+++ Web/JavaScript-DOM-And-UI/12.ExamPreparation/task2/Scripts/_references.js
@@ -0,0 +1,3 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../jquery.min.js" />
+/// <reference path="../scripts.js" />
--- Web/JavaScript-DOM-And-UI/12.ExamPreparation/task2/scripts.js
+++ Web/JavaScript-DOM-And-UI/12.ExamPreparation/task2/scripts.js
@@ -1,3 +1,21 @@
-$.fn.tabs = function () {
+/// <reference path="Scripts/_references.js" />
 
-};
+(function ($) {
+    $.fn.tabs = function () {
+        var $this = $(this);
+        $this.addClass('tabs-container');
+        hideTabControlContent();
+
+        $this.on('click', '.tab-item-title', function () {
+            var $tabControl = $(this);
+            hideTabControlContent();
+            $this.find('.tab-item').removeClass('current');
+            var $next = $tabControl.next().show();
+            $next.parent().addClass('current');
+        });
+
+        function hideTabControlContent() {
+            $this.find('.tab-item-content').hide();
+        }
+    };
+}(jQuery));

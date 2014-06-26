4ee80f7bad2f2c463041b8c66570fc75708276e0
Martin Anastasov <m4rt00@gmail.com>
2014-06-26 14:51:53 +0300

    update

---
+++ Programming/High-Quality-Code/15.DevelopmentTools/SimpleT4Template/T4SimpleClass.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleT4Template
+{
+    using System;
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}

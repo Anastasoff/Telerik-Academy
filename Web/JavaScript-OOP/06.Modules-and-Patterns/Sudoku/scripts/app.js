6c74100906df46450e8533d24bf9e5d4065e9181
Martin Anastasov <m4rt00@gmail.com>
2014-07-05 20:56:51 +0300

    Zeus update

---
+++ Web/JavaScript-OOP/06.Modules-and-Patterns/Sudoku/scripts/app.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+    require.config({
+        paths: {
+            'jquery': 'bower_components/jquery/dist/jquery'
+        }
+    });
+
+    require(['jquery', 'sudoku'], function ($, Sudoku) {
+        var game = Sudoku.getInstance();
+        $('#container').append(game.getGameBoard());
+
+        $('#solve').click(function () {
+            game.solve();
+        });
+        $('#validate').click(function () {
+            game.validate();
+        });
+        $('#reset').click(function () {
+            game.reset();
+        });
+    });
+}());

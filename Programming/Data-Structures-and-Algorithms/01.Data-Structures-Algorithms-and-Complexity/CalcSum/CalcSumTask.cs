cb52454759a228fa7168e4be3b5ba47ff42c9a45
Martin Anastasov <m4rt00@gmail.com>
2014-08-11 20:46:46 +0300

    Added Random genereted matrix to text the CalcSum method

--- Programming/Data-Structures-and-Algorithms/01.Data-Structures-Algorithms-and-Complexity/CalcSum/CalcSumTask.cs
+++ Programming/Data-Structures-and-Algorithms/01.Data-Structures-Algorithms-and-Complexity/CalcSum/CalcSumTask.cs
@@ -7,15 +7,38 @@
         {
             long sum = 0;
             for (int col = 0; col < matrix.GetLength(0); col++)
-                sum += matrix[row, col];
+            {
+                
+sum += matrix[row, col];
+        }
             if (row + 1 < matrix.GetLength(1))
                 sum += CalcSum(matrix, row + 1);
             return sum;
         }
 
+        public static int[,] GenerateRandomMatrix(int n, int m)
+        {
+            Random randomGenerator = new Random();
+            int[,] generatedMatrix = new int[n, m];
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    int currentCellValue = randomGenerator.Next(n * m * 2);
+                    generatedMatrix[row, col] = currentCellValue;
+                }
+            }
+
+            return generatedMatrix;
+        }
+
         public static void Main()
         {
-            // Console.WriteLine(CalcSum(matrix, 0));
+            int n = 10;
+            int m = 5;
+            int[,] matrix = GenerateRandomMatrix(n, m);
+            Console.WriteLine(CalcSum(matrix, 0));
         }
     }
 }

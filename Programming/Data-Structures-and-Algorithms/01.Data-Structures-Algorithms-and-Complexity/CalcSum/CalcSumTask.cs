1f838d8580c843936f353812407bf633bb9d8280
Martin Anastasov <m4rt00@gmail.com>
2014-08-12 10:25:16 +0300

    Update

--- Programming/Data-Structures-and-Algorithms/01.Data-Structures-Algorithms-and-Complexity/CalcSum/CalcSumTask.cs
+++ Programming/Data-Structures-and-Algorithms/01.Data-Structures-Algorithms-and-Complexity/CalcSum/CalcSumTask.cs
@@ -1,6 +1,7 @@
 ﻿namespace CalcSum
 {
     using System;
+
     public class CalcSumTask
     {
         public static long CalcSum(int[,] matrix, int row)
@@ -8,11 +9,14 @@
             long sum = 0;
             for (int col = 0; col < matrix.GetLength(0); col++)
             {
-                
-sum += matrix[row, col];
-        }
+                sum += matrix[row, col];
+            }
+
             if (row + 1 < matrix.GetLength(1))
+            {
                 sum += CalcSum(matrix, row + 1);
+            }
+
             return sum;
         }
 

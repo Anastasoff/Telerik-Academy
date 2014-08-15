c37815f01e0afb858861052726451f6106202bd4
Martin Anastasov <m4rt00@gmail.com>
2014-08-15 14:34:19 +0300

    Added SumAndAverageOfSequence task

---
+++ Programming/Data-Structures-and-Algorithms/02.Linear-Data-Structures/SumAndAverageOfSequence/SumAndAverageOfSequence.cs
@@ -0,0 +1,56 @@
+﻿namespace SumAndAverageOfSequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SumAndAverageOfSequence
+    {
+        public static IList<int> ProcessInput()
+        {
+            IList<int> inputSequence = new List<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == string.Empty || input == "end")
+                {
+                    break;
+                }
+
+                int currentInteger = int.Parse(input);
+                inputSequence.Add(currentInteger);
+            }
+
+            return inputSequence;
+        }
+
+        public static int CalculateSum(IList<int> sequence)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                sum += sequence[i];
+            }
+
+            return sum;
+        }
+
+        public static int CalculateAverage(int sum, int count)
+        {
+            return sum / count;
+        }
+
+        public static void Main(string[] args)
+        {
+            IList<int> sequence = ProcessInput();
+
+            int sum = CalculateSum(sequence);
+            Console.WriteLine("Sum: " + sum);
+
+            int average = CalculateAverage(sum, sequence.Count);
+            Console.WriteLine("Average: " + average);
+        }
+    }
+}

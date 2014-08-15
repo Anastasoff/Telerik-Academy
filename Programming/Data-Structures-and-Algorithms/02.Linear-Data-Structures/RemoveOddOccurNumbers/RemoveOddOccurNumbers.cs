052b077734af42dd1aa9726f7ee3318cb143eec2
Martin Anastasov <m4rt00@gmail.com>
2014-08-15 19:30:30 +0300

    Added RemoveOddOccurNumbers task

---
+++ Programming/Data-Structures-and-Algorithms/02.Linear-Data-Structures/RemoveOddOccurNumbers/RemoveOddOccurNumbers.cs
@@ -0,0 +1,48 @@
+﻿// 6. Write a program that removes from given sequence all numbers that occur odd number of times.
+namespace RemoveOddOccurNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class RemoveOddOccurNumbers
+    {
+        public static List<int> FindAndRemoveOddOccurNumbers(List<int> sequence)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                int currentNumber = sequence[i];
+                if (numbers.ContainsKey(currentNumber))
+                {
+                    numbers[currentNumber]++;
+                }
+                else
+                {
+                    numbers[currentNumber] = 1;
+                }
+            }
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                int currentNumber = sequence[i];
+                if (numbers.ContainsKey(currentNumber) && numbers[currentNumber] % 2 == 0)
+                {
+                    result.Add(sequence[i]);
+                }
+            }
+
+            return result;
+        }
+
+        private static void Main(string[] args)
+        {
+            List<int> sequence = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+            Console.WriteLine(string.Join(", ", sequence));
+
+            List<int> result = FindAndRemoveOddOccurNumbers(sequence);
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}

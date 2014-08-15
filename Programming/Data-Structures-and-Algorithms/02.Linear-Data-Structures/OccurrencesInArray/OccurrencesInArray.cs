58009324827f791acf6d0897c111c937737c8fd1
Martin Anastasov <m4rt00@gmail.com>
2014-08-15 19:50:01 +0300

    Added OccurrencesInArray task

---
+++ Programming/Data-Structures-and-Algorithms/02.Linear-Data-Structures/OccurrencesInArray/OccurrencesInArray.cs
@@ -0,0 +1,40 @@
+﻿// 7. Write a program that finds in given array of integers (all belonging to the range [0..1000]) how many times each of them occurs.
+namespace OccurrencesInArray
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class OccurrencesInArray
+    {
+        public static SortedDictionary<int, int> FindOccurrences(int[] array)
+        {
+            var occurrences = new SortedDictionary<int, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentInteger = array[i];
+                if (occurrences.ContainsKey(currentInteger))
+                {
+                    occurrences[currentInteger]++;
+                }
+                else
+                {
+                    occurrences[currentInteger] = 1;
+                }
+            }
+
+            return occurrences;
+        }
+
+        private static void Main(string[] args)
+        {
+            int[] array = { 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+
+            var occurrences = FindOccurrences(array);
+            foreach (var item in occurrences)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value + " times");
+            }
+        }
+    }
+}

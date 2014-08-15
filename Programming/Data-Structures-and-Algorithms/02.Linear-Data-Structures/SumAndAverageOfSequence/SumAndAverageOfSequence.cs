6390d875050965a26d464b4708a177a9c54229a2
Martin Anastasov <m4rt00@gmail.com>
2014-08-15 14:46:29 +0300

    Update

--- Programming/Data-Structures-and-Algorithms/02.Linear-Data-Structures/SumAndAverageOfSequence/SumAndAverageOfSequence.cs
+++ Programming/Data-Structures-and-Algorithms/02.Linear-Data-Structures/SumAndAverageOfSequence/SumAndAverageOfSequence.cs
@@ -1,4 +1,8 @@
-﻿namespace SumAndAverageOfSequence
+﻿// 1. Write a program that reads from the console a sequence of positive integer numbers.
+//  The sequence ends when empty line is entered.
+//  Calculate and print the sum and average of the elements of the sequence.
+//  Keep the sequence in List<int>.
+namespace SumAndAverageOfSequence
 {
     using System;
     using System.Collections.Generic;

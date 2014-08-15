f5b54773790e198a3de4fe2efacec57b581e8d81
Martin Anastasov <m4rt00@gmail.com>
2014-08-15 14:45:13 +0300

    Added ReverseInputIntegers task

---
+++ Programming/Data-Structures-and-Algorithms/02.Linear-Data-Structures/ReverseInputIntegers/ReverseInputIntegers.cs
@@ -0,0 +1,40 @@
+﻿// 2. Write a program that reads N integers from the console and reverses them using a stack.
+//  Use the Stack<int> class.
+namespace ReverseInputIntegers
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class ReverseInputIntegers
+    {
+        public static Stack<int> ProcessInput()
+        {
+            Stack<int> integers = new Stack<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == string.Empty || input == "end")
+                {
+                    break;
+                }
+
+                int currentInteger = int.Parse(input);
+                integers.Push(currentInteger);
+            }
+
+            return integers;
+        }
+
+        private static void Main(string[] args)
+        {
+            Stack<int> integers = ProcessInput();
+
+            while (integers.Count > 0)
+            {
+                Console.WriteLine(integers.Pop());
+            }
+        }
+    }
+}

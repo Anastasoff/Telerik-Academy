6905a50ebcc95ca9a57cbfcf5c95ba1d023f0f9d
Martin Anastasov <m4rt00@gmail.com>
2014-08-15 17:54:46 +0300

    Added RemoveNegativeNumbers task

---
+++ Programming/Data-Structures-and-Algorithms/02.Linear-Data-Structures/RemoveNegativeNumbers/RemoveNegativeNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace RemoveNegativeNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class RemoveNegativeNumbers
+    {
+        public static void FindAndRemoveNegativeNumbers<T>(LinkedList<T> sequence) where T : IComparable<T>
+        {
+            sequence.RemoveAll(n => n.CompareTo((T)Convert.ChangeType(0, typeof(T))) < 0);
+        }
+
+        private static void Main(string[] args)
+        {
+            var sequence = new LinkedList<int>();
+
+            Random randomNumber = new Random();
+            int length = randomNumber.Next(20, 30);
+            for (int i = 0; i < length; i++)
+            {
+                int currentNumber = randomNumber.Next(-100, 100);
+                sequence.AddLast(currentNumber);
+            }
+
+            Console.WriteLine(string.Join(", ", sequence));
+
+            FindAndRemoveNegativeNumbers(sequence);
+
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+    }
+}

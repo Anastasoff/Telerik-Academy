55b6001f293bc17f26e96b44455e875c96e76ea7
Martin Anastasov <m4rt00@gmail.com>
2014-08-29 21:33:48 +0300

    Added TreeOperations task

---
+++ Programming/Data-Structures-And-Algorithms/03.Trees-and-Traversals/TreeOperations/Node.cs
@@ -0,0 +1,30 @@
+﻿namespace TreeOperations
+{
+    using System.Collections.Generic;
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public List<Node<T>> Children { get; private set; }
+
+        public bool HasParent { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+            this.Children = new List<Node<T>>();
+        }
+
+        public void AddChild(Node<T> child)
+        {
+            child.HasParent = true;
+            this.Children.Add(child);
+        }
+
+        public Node<T> GetChild(int index)
+        {
+            return this.Children[index];
+        }
+    }
+}

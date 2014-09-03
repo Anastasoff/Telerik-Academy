97ad95476f360e449e9f47222b886ed7ac03d4d4
Martin Anastasov <m4rt00@gmail.com>
2014-09-03 16:32:47 +0300

    Added StudentsInCourses task

---
+++ Programming/Data-Structures-And-Algorithms/06.Data-Structures-Efficiency/StudentsInCourses/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentsInCourses
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
---
+++ Programming/Data-Structures-And-Algorithms/06.Data-Structures-Efficiency/StudentsInCourses/StudentsInCourses.cs
@@ -0,0 +1,45 @@
+﻿namespace StudentsInCourses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    internal class StudentsInCourses
+    {
+        private static void Main(string[] args)
+        {
+            string filePath = "../.../students.txt";
+            var courses = new SortedDictionary<string, List<Student>>();
+            foreach (string line in File.ReadLines(filePath))
+            {
+                var fields = line.Split('|')
+                    .Select(s => s.Trim())
+                    .ToArray();
+                var fname = fields[0];
+                var lname = fields[1];
+                var course = fields[2];
+
+                List<Student> students;
+                if (!courses.TryGetValue(course, out students))
+                {
+                    students = new List<Student>();
+                    courses.Add(course, students);
+                }
+
+                var student = new Student()
+                {
+                    FirstName = fname,
+                    LastName = lname
+                };
+                students.Add(student);
+            }
+            foreach (var course in courses)
+            {
+                var orderedStr = course.Value.OrderBy(s => s.LastName).ThenBy(s => s.FirstName);
+                string formatedStr = string.Format("{0}: {1}", course.Key, string.Join(", ", orderedStr));
+                Console.WriteLine(formatedStr);
+            }
+        }
+    }
+}

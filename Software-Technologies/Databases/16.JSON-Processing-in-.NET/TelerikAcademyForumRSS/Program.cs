4ff2a86e5f8c8fde3d9ef505cf0c4b7e76e4e447
Martin Anastasov <m4rt00@gmail.com>
2014-09-07 20:07:59 +0300

    Added JSON Processing homework

---
+++ Software-Technologies/Databases/16.JSON-Processing-in-.NET/TelerikAcademyForumRSS/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace TelerikAcademyForumRSS
+{
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Xml.Linq;
+
+    public class Program
+    {
+        private const string RssUrl = "http://forums.academy.telerik.com/feed/qa.rss";
+        private const string RssFilePath = "../../RSS/rss.xml";
+        private const string JsonFilePath = "../../RSS/rss.json";
+
+        public static void Main(string[] args)
+        {
+            DownloadRssFeedFile(new WebClient());
+
+            string json = ParseXmlRssToJson();
+
+            PrintAllQuestionTitles(json);
+
+            var items = ParseJsonToPoco(json);
+        }
+
+        private static void DownloadRssFeedFile(WebClient webClient)
+        {
+            using (webClient)
+            {
+                webClient.DownloadFile(RssUrl, RssFilePath);
+            }
+        }
+
+        private static string ParseXmlRssToJson()
+        {
+            var rssXml = XDocument.Load(RssFilePath);
+            string rssJson = JsonConvert.SerializeXNode(rssXml, Formatting.Indented);
+            File.WriteAllText(JsonFilePath, rssJson);
+
+            return rssJson;
+        }
+
+        private static void PrintAllQuestionTitles(string json)
+        {
+            var jsonObj = JObject.Parse(json);
+            var titles = jsonObj["rss"]["channel"]["item"].Select(i => i["title"]);
+            Console.WriteLine(string.Join(Environment.NewLine, titles));
+        }
+
+        private static RssItem[] ParseJsonToPoco(string json)
+        {
+            var jsonObj = JObject.Parse(json);
+            var itemsJson = jsonObj["rss"]["channel"]["item"].ToString();
+            var items = JsonConvert.DeserializeObject<RssItem[]>(itemsJson);
+
+            return items;
+        }
+    }
+}

b1c06b5f060fab29c6259b2894e6cb09d931862f
Martin Anastasov <m4rt00@gmail.com>
2014-09-10 19:31:35 +0300

    Added Combinatorics homework.

---
+++ Programming/Data-Structures-And-Algorithms/09.Combinatorics/BinaryPasswords/BinaryPasswords.cs
@@ -0,0 +1,29 @@
+﻿namespace BinaryPasswords
+{
+    using System;
+
+    public class BinaryPasswords
+    {
+        public static void Main()
+        {
+            string password = Console.ReadLine();
+
+            int missingDigits = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] == '*')
+                {
+                    missingDigits++;
+                }
+            }
+
+            long numberOfPossiblePasswords = 1;
+            for (int i = 0; i < missingDigits; i++)
+            {
+                numberOfPossiblePasswords *= 2;
+            }
+
+            Console.WriteLine(numberOfPossiblePasswords);
+        }
+    }
+}
---
+++ Programming/Data-Structures-And-Algorithms/09.Combinatorics/ColoredBunny/ColoredBunny.cs
@@ -0,0 +1,33 @@
+﻿namespace ColoredBunny
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ColoredBunny
+    {
+        public static void Main()
+        {
+            Dictionary<int, int> bunnies = new Dictionary<int, int>();
+            int numberOfBunnies = int.Parse(Console.ReadLine());
+            long result = 0;
+
+            for (int i = 0; i < numberOfBunnies; i++)
+            {
+                int answer = int.Parse(Console.ReadLine()) + 1;
+                if (!bunnies.ContainsKey(answer))
+                {
+                    bunnies[answer] = 0;
+                }
+
+                bunnies[answer]++;
+            }
+
+            foreach (var bunny in bunnies)
+            {
+                result += (bunny.Key + bunny.Value - 1) / bunny.Key * bunny.Key;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

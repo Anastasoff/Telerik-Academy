db970e772e6f863b6e814925d6b5098f12aa8e59
Martin Anastasov <m4rt00@gmail.com>
2014-09-12 23:39:48 +0300

    up

---
+++ Programming/Data-Structures-And-Algorithms/10.Graphs-and-Graph-Algorithms/Salaries/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Salaries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}

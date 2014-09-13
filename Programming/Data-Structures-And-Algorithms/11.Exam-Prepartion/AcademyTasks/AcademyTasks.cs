cfd80b183e94972254d5ecad0bb10dc7bf33486c
Martin Anastasov <m4rt00@gmail.com>
2014-09-13 15:40:31 +0300

    Added Academy Tasks

--- Programming/Data-Structures-And-Algorithms/11.Exam-Prepartion/AcademyTasks/AcademyTasks.cs
+++ Programming/Data-Structures-And-Algorithms/11.Exam-Prepartion/AcademyTasks/AcademyTasks.cs
@@ -1,23 +1,28 @@
 ﻿namespace AcademyTasks
 {
     using System;
-    using System.Collections.Generic;
 
     public class AcademyTasks
     {
         private static int[] pleasantness;
         private static int variety;
+        private static int minSolvedProblems;
 
         public static void Main()
         {
             ProcessInput();
 
-            Console.WriteLine(2);
+            minSolvedProblems = pleasantness.Length;
+
+            SolveTasks(0, pleasantness[0], pleasantness[0], 1);
+
+            Console.WriteLine(minSolvedProblems);
         }
 
         private static void ProcessInput()
         {
-            string[] listOfNumbersInPleasantness = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] listOfNumbersInPleasantness = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
             int length = listOfNumbersInPleasantness.Length;
             pleasantness = new int[length];
             for (int i = 0; i < length; i++)
@@ -28,16 +33,28 @@
             variety = int.Parse(Console.ReadLine());
         }
 
-        private static void SolveTasks(int index)
+        private static void SolveTasks(int index, int min, int max, int solvedProblems)
         {
+            if (max - min >= variety)
+            {
+                minSolvedProblems = Math.Min(minSolvedProblems, solvedProblems);
+                return;
+            }
+
             if (index >= pleasantness.Length)
             {
                 return;
             }
 
-            for (int i = 0; i < pleasantness.Length; i++)
+            for (int i = 1; i <= 2; i++)
             {
-
+                int currentIndex = index + i;
+                if (currentIndex < pleasantness.Length)
+                {
+                    int currentMin = Math.Min(min, pleasantness[currentIndex]);
+                    int currentMax = Math.Max(max, pleasantness[currentIndex]);
+                    SolveTasks(currentIndex, currentMin, currentMax, solvedProblems + 1);
+                }
             }
         }
     }

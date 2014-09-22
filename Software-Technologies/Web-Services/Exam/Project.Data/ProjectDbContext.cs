c80323b915718e802e1b8ccb1446c03cbfcb29c8
Martin Anastasov <m4rt00@gmail.com>
2014-09-22 13:13:09 +0300

    Added Exam template

---
+++ Software-Technologies/Web-Services/Exam/Project.Data/ProjectDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Project.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Project.Models;
+
+    public class ProjectDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ProjectDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ProjectDbContext Create()
+        {
+            return new ProjectDbContext();
+        }
+    }
+}
---
+++ Software-Technologies/Web-Services/Exam/Project.WebAPI/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Project.WebAPI.Startup))]
+
+namespace Project.WebAPI
+{
+    using Owin;
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

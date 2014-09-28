c68f3b11be3a80ae6abb5a34d728f7cb97b84772
Martin Anastasov <m4rt00@gmail.com>
2014-09-28 18:02:19 +0300

    Added TicTacToe.WebApi project

---
+++ Software-Technologies/Web-Services/01.ASP.NET-Web-API/TicTacToe.WebApi/TicTacToe.Web/DataModels/ScoreInfoDataModel.cs
@@ -0,0 +1,15 @@
+namespace TicTacToe.Web.DataModels
+{
+    public class ScoreInfoDataModel
+    {
+        public string Username { get; set; }
+
+        public int Wins { get; set; }
+
+        public int Losses { get; set; }
+
+        public int Draws { get; set; }
+
+        public int Points { get; set; }
+    }
+}
